d85c79289c7f80124f7cf6209cd87af2052ec87b
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-04-23 02:40:57 +0900

    Add files via upload

---
+++ download2-3-3.py
@@ -0,0 +1,22 @@
+import io
+import sys
+import urllib.request as req
+from urllib.parse import urlencode
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+API = "https://api.ipify.org"
+
+values = {
+    'format' : 'json'
+}
+print('before', values)
+params = urlencode(values)
+print('after', params)
+ 
+url = API + "?" + params
+print("요청 url", url)
+
+reqData = req.urlopen(url).read().decode('utf-8')
+print("출력", reqData)
