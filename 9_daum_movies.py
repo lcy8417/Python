de762d79a50de579d444a237657807fde8119f02
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-04-25 22:35:02 +0900

    Add files via upload

---
+++ 9_daum_movies.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"}
+
+
+for year in range(2015, 2020):
+    url = f"https://search.daum.net/search?w=tot&q={year}%EB%85%84%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&DA=MOR&rtmaxcoll=MOR"
+    res = requests.get(url, headers=headers)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, 'lxml')
+
+    images = soup.find_all("img", attrs={"class":"thumb_img"})
+
+    for idx, image in enumerate(images):
+        # print(image["src"])
+        image_url = image["src"]
+        if image_url.startswith("//"):
+            image_url = "https:" + image_url
+        
+        print(image_url)
+        image_res = requests.get(image_url)
+        image_res.raise_for_status()
+
+        with open("movie_{}_{}.jpg".format(year, idx+1), "wb",) as f:
+            f.write(image_res.content) # content 이미지
+
+        if idx >= 4: # 상위 5개 까지만 다운
+            break
