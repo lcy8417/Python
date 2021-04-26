f72b63465dfc0161b529096a8432ea6df1100f28
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-04-26 23:46:16 +0900

    Add files via upload

---
+++ 3-2-3.py
@@ -0,0 +1,16 @@
+import requests, json
+
+s = requests.Session()
+
+r = s.get('http://httpbin.org/stream/20', stream=True)
+# print(r.text)
+print(r.encoding)
+# print(r.json)
+if r.encoding is None:
+    r.encoding = 'utf-8'
+
+for line in r.iter_lines(decode_unicode=True):
+    # print(line)
+    b = json.loads(line) #dict
+    for e in b.keys():
+        print("key:",e,"values:",b[e])
